5d0e205705f55ac212539f7ebbb99dd41fa5fa86
hermayani2786 <herma.yani2786@gmail.com>
2026-06-11 11:27:05 +0700

    menambahkan fungsi main

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -105,3 +105,20 @@ public:
     }
 
     void postorder(Node *ptr)
+    {
+        if (ROOT == NULL)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+
+        if (ptr != NULL)
+        {
+            postorder(ptr->leftchild);
+            postorder(ptr->rightchild);
+            cout << ptr->info << " ";
+        }
+    }
+};
+
+int main()
