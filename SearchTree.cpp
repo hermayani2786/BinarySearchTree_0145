0430afe8d78b48806041053bc71439efe3b578b8
hermayani2786 <herma.yani2786@gmail.com>
2026-06-11 11:33:10 +0700

    menambahkan switch

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -134,5 +134,12 @@ int main()
         cout << "4. Perform postorder traversal" << endl;
         cout << "5. Exit" << endl;
         cout << "\nEnter your choice (1-5): ";
+
+        char ch;
+        cin >> ch;
+
+        cout << endl;
+
+        switch (ch)
     }
 }
