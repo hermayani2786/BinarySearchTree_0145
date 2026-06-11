eb2adcaf888c092e39934cfa259f92cb0e0ab22d
hermayani2786 <herma.yani2786@gmail.com>
2026-06-11 11:04:29 +0700

    EDIT -> PROSEDUR INSERT FIX

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -29,4 +29,31 @@ public:
 
     //Insert a node in the binary search tree
     void insert(string element)
+    {
+        Node *newNode = new Node(element, NULL, NULL);
+
+        newNode->info = element;
+        newNode->leftchild = NULL;
+        newNode->rightchild = NULL;
+
+        Node *parent = NULL;
+        Node *currentNode = NULL;
+
+        search(element, parent, currentNode);
+
+        if (parent == NULL)
+        {
+            ROOT =newNode;
+            return;
+        }
+
+        if (element < parent->info)
+        {
+            parent->leftchild = newNode;
+        }
+        else if (element > parent-> info)
+        {
+            parent->rightchild = newNode;
+        }
+    }
 }
